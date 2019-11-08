5d25c337e6d7833116bddc7bfd91c7ed795a2521
Luis Carlos <lucarbass@live.com>
2019-11-08 01:22:10 -0300

    Subtotal & total methods

--- src/main/java/com/example/course/entities/OrderItem.java
+++ src/main/java/com/example/course/entities/OrderItem.java
@@ -66,6 +66,10 @@ public class OrderItem implements Serializable {
 		this.quantity = quantity;
 	}
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
